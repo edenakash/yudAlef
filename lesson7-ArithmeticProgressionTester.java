eae237ae64312406c7d307d9079821282eafdd7b
Eden Nakash <144595554+edenakash@users.noreply.github.com>
2023-12-18 22:31:22 +0200

    Update lesson7-ArithmeticProgressionTester.java

--- lesson7-ArithmeticProgressionTester.java
+++ lesson7-ArithmeticProgressionTester.java
@@ -1,19 +1,35 @@
+
 public class ArithmeticTester {
 
 	public static void main(String[] args)
     {
-        ArithmeticProgression myFunctions = new ArithmeticProgression(2, 2);
+        ArithmeticProgression myFunctions = new ArithmeticProgression(1, 1);
 
 
-
-        myFunctions.printGeneralSeries();
-        int result1 = myFunctions.memberCalculation(4);
-        float result2 = myFunctions.calculateN(4);
         
-        System.out.println("RESULT 1 = " + result1);
-        System.out.println("RESULT 2 = " + result2);
-
-
+        myFunctions.printGeneralSeries();
+        int result1 = myFunctions.memberCalculation(11);
+        float result2 = myFunctions.calculateN(5);
+        
+        System.out.println("Last member in this Progression: " + result1);
+        System.out.println("Value of member: " + result2);
+        
+        // add 7,8,9 from arithmeticClass
+        myFunctions.setFM(5);
+        myFunctions.setDiff(2);
+        
+        System.out.println("New first member: " + myFunctions.getFM());
+        System.out.println("New difference: " + myFunctions.getDiff());
+        
+        ArithmeticProgression otherProgression = new ArithmeticProgression(myFunctions);
+        
+        System.out.println("Are progressions equal? " + myFunctions.equal(otherProgression));
+        
+        int diffToCompare = 5;
+        System.out.println("Is the difference smaller than " + diffToCompare + "? " + myFunctions.smallDiff(diffToCompare));
+    
+       
+        
     }
 
-}
+	}
