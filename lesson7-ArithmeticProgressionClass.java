f89b96be5eef587c480fe0edeb8848ea8a642148
Eden Nakash <144595554+edenakash@users.noreply.github.com>
2023-12-18 22:30:41 +0200

    Update lesson7-ArithmeticProgressionClass.java

--- lesson7-ArithmeticProgressionClass.java
+++ lesson7-ArithmeticProgressionClass.java
@@ -1,3 +1,4 @@
+
 public class ArithmeticProgression {
 	private int _firstMember;
 	private int _difference;
@@ -10,13 +11,36 @@ public class ArithmeticProgression {
 		_difference = diff;
 	
 	}
+	
+	public int getFM() {
+		return _firstMember;
+	}
+	
+	public int getDiff() {
+		return _difference;
+	}
+	
+	public void setFM(int fM) {
+		_firstMember = fM;
+		
+	}
+	public void setDiff(int diff) {
+		_difference = diff;
+	}
+	
+	public ArithmeticProgression(ArithmeticProgression other) {
+		_firstMember = other._firstMember;
+		_difference = other._difference;
+	}
+	
+	
 	public void printGeneralSeries() {
-		System.out.println("an = "+_firstMember + _difference + "(n-1)");
+		System.out.println("an = a1 + d(n-1)");
 	}
 	
 	public int memberCalculation(int n)
 	{
-		int an= _firstMember + (_difference*n - _difference);
+		int an= _firstMember + _difference*(n-1);
 		return an;
 	}
 	
@@ -27,13 +51,28 @@ public class ArithmeticProgression {
 			float n = (an - fM + diff) / diff + 1;
 			
 			if (n >= 1 && n % 1 == 0) {
-				return (int) n - 1;
+				return (int) n;
 			}
 			else {
 				return 0;
 			}
+	}	
+	
+	public boolean equal(ArithmeticProgression other) {
+		if((_firstMember == other._firstMember) && (_difference == other._difference))
+			return true;
+		else
+			return false;
 		
 	}
+	
+	public boolean smallDiff(int diff) {
+			return diff < _difference;
+		
+	}
+	//add to arithmetic tester
+		
 
-  
 }
+
+
